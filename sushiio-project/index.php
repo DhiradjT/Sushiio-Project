9346c18312627062b5eee09dac94c9d7e8a788af
Dhiradj Tangali <90622013+DhiradjT@users.noreply.github.com>
2022-09-07 15:02:56 +0200

    navbar + cards gemaakt op de homepage

---
+++ sushiio-project/index.php
@@ -0,0 +1,96 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>Sushiio</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    </head>
+    <body>
+        
+        <div class="col-lg-12">
+        
+    <nav class="navbar navbar-expand-lg bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand text-light" href="#">Sushiio</a>
+         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+         <div class="navbar-nav">
+         <a class="nav-link active text-light" aria-current="page" href="#">Home</a>
+         <a class="nav-link text-light" href="#">Bestellen</a>
+      </div>
+    </div>
+  </div>
+</nav>
+        </div>
+
+
+
+
+
+
+<div class="heroimg">
+    <img src="img/banner2.jpg" class="img-fluid" alt="sushibanner">
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <div class="container">
+            <div class="col-md-12 text-center">
+                <h2>Goedemiddag, welkom bij Sushiio</h2>
+                <p class="text-secondary">
+                Wij zijn gespecialiseerd in de Japanse keuken <br>
+                Het woord "sushi" is afkomstig van "su", wat azijn betekent en "shi" -- rijst.
+                </p>
+
+                <p class="fw-bold text-muted">
+                Vandaag dinsdag 23 augustus 2022 <br>
+                Bezorgtijd vanaf nu: 18:00
+                </p>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+
+            <div class="col-md-6">
+
+            <div class="card shadow border-0" style="width: 30rem;">
+              <img src="img/sushibowl.jpg" class="card-img-top img-fluid" alt="variety of sushi">
+              <div class="card-body">
+              <p class="card-text text-center fw-bold">Bestel bij ons je sushi's</p>
+            </div>
+          </div>
+       </div> <!---End col div--->
+
+
+            <div class="col-md-6">
+
+            <div class="card shadow border-0" style="width: 30rem;">
+              <img src="img/sushibowl.jpg" class="card-img-top img-fluid" alt="variety of sushi">
+              <div class="card-body">
+              <p class="card-text text-center fw-bold">Keuze uit verschillende soorten sushi's</p>
+            </div>
+          </div>
+      </div> <!---End col div--->
+
+            </div> <!-- End row DIV -->
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    </body>
+    </html>
