d773a911caba5c139c69ad286c66a063ecf53af9
Dhiradj <dhiradj2004@gmail.com>
2022-09-09 01:51:29 +0200

    home pagina afgemaakt cards + footer

--- sushiio-project/index.php
+++ sushiio-project/index.php
@@ -10,85 +10,113 @@
     </head>
     <body>
         
-        <div class="col-lg-12">
-        
-    <nav class="navbar navbar-expand-lg bg-dark">
-      <div class="container-fluid">
-        <a class="navbar-brand text-light" href="#">Sushiio</a>
-         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-         </button>
-         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-         <div class="navbar-nav">
-         <a class="nav-link active text-light" aria-current="page" href="#">Home</a>
-         <a class="nav-link text-light" href="#">Bestellen</a>
-      </div>
-    </div>
-  </div>
-</nav>
+
+    <!-- Nav section  -->
+
+      <div class="col-lg-12">
+        <nav class="navbar navbar-expand-lg bg-dark">
+         <div class="container-fluid">
+           <a class="navbar-brand text-light" href="#">SUSHIIO</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+            <a class="nav-link active text-light" aria-current="page" href="#">Home</a>
+            <a class="nav-link text-light" href="order.php">Bestellen</a>
+          </div>
+         </div>
         </div>
+      </nav>
+     </div> <!-- end col  -->
+
+
+     <!-- Header section  -->
+
+      <div class="p-5 bg-image" class="img-fluid" style="background-image: url('./img/test2.png'); height: 310px;" ">
+      </div>
 
 
 
 
-
-
-<div class="heroimg">
-    <img src="img/banner2.jpg" class="img-fluid" alt="sushibanner">
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
+    <!-- Header text section  -->
 
         <div class="container">
+          <div class="row">
             <div class="col-md-12 text-center">
-                <h2>Goedemiddag, welkom bij Sushiio</h2>
-                <p class="text-secondary">
+              <h2>Goedemiddag, welkom bij <b>Sushiio</b></h2>
+              <p class="text-secondary">
                 Wij zijn gespecialiseerd in de Japanse keuken <br>
                 Het woord "sushi" is afkomstig van "su", wat azijn betekent en "shi" -- rijst.
                 </p>
-
-                <p class="fw-bold text-muted">
+              <p class="fw-bold text-muted">
                 Vandaag dinsdag 23 augustus 2022 <br>
                 Bezorgtijd vanaf nu: 18:00
-                </p>
+              </p>
             </div>
-        </div>
+        </div>  <!-- end row  -->
+        </div> <!-- end container  -->
+
+
+        <!-- Cards section  -->
 
         <div class="container">
-            <div class="row">
+          <div class="row">
 
-            <div class="col-md-6">
+          <div class="col">
+          <div class="card shadow border-0" style="">
+          <img src="./img/testdel2.webp" class="card-img-top" alt="food delivery">
+          <div class="card-body">
+          <p class="card-text">Bestel bij ons je sushi's.</p>
+        </div>
+      </div>
+    </div>
 
-            <div class="card shadow border-0" style="width: 30rem;">
-              <img src="img/sushibowl.jpg" class="card-img-top img-fluid" alt="variety of sushi">
-              <div class="card-body">
-              <p class="card-text text-center fw-bold">Bestel bij ons je sushi's</p>
-            </div>
+          <div class="col">
+          <div class="card shadow border-0" style="">
+          <img src="./img/sushi-3.jpg" class="card-img-top" alt="a bowl of sushi">
+          <div class="card-body">
+          <p class="card-text">Keuze uit verschillende soorten sushi's.</p>
+        </div>
+      </div>
           </div>
-       </div> <!---End col div--->
+          </div>  <!-- end row  -->
+        </div> <!-- end container  -->
 
 
-            <div class="col-md-6">
+        <div class="container-fluid bg-dark">
+            <div class="row mt-5">
 
-            <div class="card shadow border-0" style="width: 30rem;">
-              <img src="img/sushibowl.jpg" class="card-img-top img-fluid" alt="variety of sushi">
-              <div class="card-body">
-              <p class="card-text text-center fw-bold">Keuze uit verschillende soorten sushi's</p>
+            <div class="col mt-3 text-light text-center">
+              <p>
+              <b>Contactgegevens</b><br>
+                Restaurant Sushiio<br>
+                Appelstraat 1<br>
+                1111 AA Fruit<br>
+                sushiio@mail.io<br>
+                06-12345678<br>
+              </p>
             </div>
-          </div>
-      </div> <!---End col div--->
 
-            </div> <!-- End row DIV -->
+
+            <div class="col mt-3 text-light text-center">
+            <p>
+              <b>Openingstijden</b><br>
+                Maandag: Gesloten<br>
+                Dinsdag: 16:00-22:00<br>
+                Woensdag: 16:00-22:00<br>
+                Donderdag: 16:00-22:00<br>
+                Vrijdag: 15:00-22:00<br>
+                Zaterdag: 15:00-22:00<br>
+                Zondag: 15:00-22:00<br>
+              </p>
+            </div>
+
+
+            </div>
+        </div> 
+ 
+        
 
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
